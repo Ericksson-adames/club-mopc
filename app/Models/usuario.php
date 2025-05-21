e608cede09ec31cdb5b36ef4d4808a5b215e3bfd
Erickson Adames <ericksonadamesabreu@gmail.com>
2025-05-21 17:42:18 -0400

    ultima actualizacion

---
+++ app/Models/usuario.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+
+// clase para indicar que este sera mi usuario de autenticacion
+class Usuario extends Authenticatable implements FilamentUser
+{
+    use Notifiable;
+
+     // indico a filament que esta es mi tabla en la base de datos 
+    protected $table = 'usuario';
+
+     // indico a filament que esta es mi llave primaria en la base de datos 
+  //  protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'email',
+        'password',
+        'id_rol',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    //es un accessor de Eloquent en Laravel que crea una propiedad virtual llamada name.
+    public function getNameAttribute(): string
+    {
+        return $this->nombre ?? 'Sin nombre';
+    }
+
+    //es para específica a Filament y cumple el propósito de mostrar el nombre del usuario en la interfaz del panel.
+    public function getFilamentName(): string
+    {
+        return $this->nombre ?? 'Sin nombre';
+    }
+    
+    //es para cuando tu modelo de usuario usa un campo de contraseña con un nombre distinto al predeterminado de Laravel, que es password 
+    //(lo tenia diferente, pero lo cambie y lo deje como quiera)
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+    
+    //es requerida por Filament cuando tu modelo implementa la interfaz (Filament\Models\Contracts\FilamentUser)
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true;
+    }
+}
